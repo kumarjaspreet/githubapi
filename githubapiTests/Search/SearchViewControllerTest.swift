0cd2adc0a8e6744a5163e25f48a7cdfa05a300d0
Jaspreet Kumar <kumar.jaspreet87@gmail.com>
2019-06-16 20:57:22 -0400

    Added inital test classes

---
+++ githubapiTests/Search/SearchViewControllerTest.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewControllerTest.swift
+//  githubapiTests
+//
+//  Created by Jaspreet Kumar. on 6/16/19.
+//  Copyright © 2019 Jaspreet Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import githubapi
+
+class SearchViewControllerTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
---
+++ githubapiTests/Search/SearchViewModelTest.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewModelTest.swift
+//  githubapiTests
+//
+//  Created by Jaspreet Kumar on 6/16/19.
+//  Copyright © 2019 Jaspreet Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import githubapi
+
+class SearchViewModelTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
